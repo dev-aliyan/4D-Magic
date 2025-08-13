16c7e58eb0380e49efc276f20048b13adbbfa5cd
aliyan77 <ahmed.aliyan774@gmail.com>
2025-08-13 21:11:49 +0500

    validator

--- Tasks/routing/src/app/core/validators/future-date-validator.ts
+++ Tasks/routing/src/app/core/validators/future-date-validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export function futureDateValidator(control: AbstractControl) : ValidationErrors | null {
     if(!control.value) return null
-
+    //Future Date Validator form will be invalid if the date is not future date
     const today = new Date();
     const selected = new Date(control.value);
     return selected > today ? null : { futureDate : true }
