b3ddcb20b1a28dd95b6dd269c69d971587d83ff1
aliyan77 <ahmed.aliyan774@gmail.com>
2025-08-27 17:02:48 +0500

    filter operator

--- Exercises/map-filter-operator/src/app/app.component.ts
+++ Exercises/map-filter-operator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter, from, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'map-filter-operator';
+
+  data : any[] = [];
+
+  myObservable = from([2,4,6,8,10,12]);
+  transformedData = this.myObservable.pipe(map((val) => {
+    return val * 5;
+  }));
+
+  filteredData = this.transformedData.pipe(filter((val) => {
+    return val % 4 === 0;
+  }));
+
+  asyncData() {
+    this.filteredData.subscribe({
+      next : (val: any) => {
+        this.data.push(val);
+        console.log(this.data);
+      }
+    });
+  }
 }
