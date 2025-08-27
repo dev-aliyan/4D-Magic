98d10c472e62fb4214a99a2da15a25c15d83ce6e
aliyan77 <ahmed.aliyan774@gmail.com>
2025-08-27 15:03:22 +0500

    changes

--- Exercises/map-filter-operator/src/app/app.component.spec.ts
+++ Exercises/map-filter-operator/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'state-management'`, () => {
+  it(`should have as title 'map-filter-operator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('state-management');
+    expect(app.title).toEqual('map-filter-operator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, state-management');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, map-filter-operator');
   });
 });
